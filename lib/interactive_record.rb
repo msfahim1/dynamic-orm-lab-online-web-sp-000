e0f57c46118e7c97ead0175a570a95ad35a463d0
Michael Fahim <michael_fahim@yahoo.com>
2020-04-12 16:58:00 +0000

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -3,4 +3,96 @@ require 'active_support/inflector'
 
 class InteractiveRecord
   
+def self.table_name
+    self.to_s.downcase.pluralize
+  end
+
+  def self.column_names
+    DB[:conn].results_as_hash = true
+
+    sql = "pragma table_info('#{table_name}')"
+
+    table_info = DB[:conn].execute(sql)
+    column_names = []
+    table_info.each do |row|
+      column_names << row["name"]
+    end
+    column_names.compact
+  end
+
+  def initialize(options={})
+    options.each do |property, value|
+      self.send("#{property}=", value)
+    end
+  end
+
+  def save
+    sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
+    DB[:conn].execute(sql)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+  end
+
+  def table_name_for_insert
+    self.class.table_name
+  end
+
+  def values_for_insert
+    values = []
+    self.class.column_names.each do |col_name|
+      values << "'#{send(col_name)}'" unless send(col_name).nil?
+    end
+    values.join(", ")
+  end
+
+  def col_names_for_insert
+    self.class.column_names.delete_if {|col| col == "id"}.join(", ")
+  end
+
+  def self.find_by_name(name)
+    sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
+    DB[:conn].execute(sql)
+  end
+  
+# for the find_by method:
+  def self.attrs_as_args(attribute_hash)
+    attributes = []
+    attribute_hash.each do |k, v|
+      attributes << k
+    end
+    attributes
+  end
+
+  def self.values_as_args(attribute_hash)
+    values = []
+    attribute_hash.each do |k, v|
+      values << v
+    end
+    values
+  end
+
+  def self.find_by(attribute_hash)
+   sql = "SELECT * FROM #{self.table_name} WHERE #{self.attrs_as_args(attribute_hash)[0]} = '#{self.values_as_args(attribute_hash)[0]}'"
+   DB[:conn].execute(sql)
+  end
+
 end
+
+
+
+
+
+
+
+require_relative "../config/environment.rb"
+require 'active_support/inflector'
+require 'interactive_record.rb'
+
+class Student < InteractiveRecord
+
+  self.column_names.each do |col_name|
+    attr_accessor col_name.to_sym
+  end
+
+end
+
+
